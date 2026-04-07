561be35abd021dbe8494654b0226cee84873e0c8
za0012 <zinazeek@gmail.com>
2026-04-07 17:08:25 +0900

    feat: 마이페이지, 문제 풀이 page파일에 복사, 변경한 대시보드 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Dashboard } from "../pages/Dashboard";
+import { Dashboard } from "../pages/dashboard/Dashboard";
 
 export const Route = createFileRoute("/")({
   component: Dashboard,
