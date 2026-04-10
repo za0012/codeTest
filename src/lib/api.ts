ae18cf9b350d80597e180c658363e3b2cb377edc
za0012 <zinazeek@gmail.com>
2026-04-10 17:48:58 +0900

    feat: 정렬 로직 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,11 @@
 // lib/api.ts
 import { supabase } from "./supabase";
-import { type Member, type Problem,type FormData, MEMBERS } from "../data/mockData"; // 기존 타입 재사용
+import {
+  type Member,
+  type Problem,
+  type FormData,
+  MEMBERS,
+} from "../data/mockData"; // 기존 타입 재사용
 
 // 멤버 전체 조회
 export const getMembers = async (): Promise<Member[]> => {
@@ -17,7 +22,7 @@ export const getProblems = async (): Promise<Problem[]> => {
   const { data, error } = await supabase
     .from("problems")
     .select("*, members(name, emoji)")
-    // .order("date", { ascending: false });
+    .order("created_at", { ascending: false });
   if (error) throw error;
   return data;
 };
@@ -27,7 +32,8 @@ export const addProblem = async (
   problem: Omit<FormData, "id" | "created_at">,
   // problem: FormData,
 ) => {
-  problem.solver_name = MEMBERS.find((p) => p.name === data.solver_name)?.id || 1;
+  problem.solver_name =
+    MEMBERS.find((p) => p.name === data.solver_name)?.id || 1;
   const { data, error } = await supabase
     .from("problems")
     .insert(problem)
