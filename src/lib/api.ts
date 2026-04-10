92c9da556a0e33b69b8cfeec2a9f7bbcb2e956ff
za0012 <zinazeek@gmail.com>
2026-04-10 17:16:01 +0900

    feat: add 시 member의 name을 id로 교체

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
 import { supabase } from "./supabase";
-import type { Member, Problem,FormData } from "../data/mockData"; // 기존 타입 재사용
+import { type Member, type Problem,type FormData, MEMBERS } from "../data/mockData"; // 기존 타입 재사용
 
 // 멤버 전체 조회
 export const getMembers = async (): Promise<Member[]> => {
@@ -24,9 +24,10 @@ export const getProblems = async (): Promise<Problem[]> => {
 
 // 문제 추가
 export const addProblem = async (
-  // problem: Omit<FormData, "id" | "created_at">,
-  problem: FormData,
+  problem: Omit<FormData, "id" | "created_at">,
+  // problem: FormData,
 ) => {
+  problem.solver_name = MEMBERS.find((p) => p.name === data.solver_name)?.id || 1;
   const { data, error } = await supabase
     .from("problems")
     .insert(problem)
