18bb66e4fd2a3571b741ceac62d9401422904432
za0012 <zinazeek@gmail.com>
2026-04-10 17:16:43 +0900

    ✨ feat: 라밸 컴포넌트 추가

---
+++ src/components/custom/LabelC.tsx
@@ -0,0 +1,18 @@
+import type { PropsWithChildren } from "react";
+
+interface labelProps {
+  title: string;
+  // children: React.ReactNode;
+  // PropsWithChildren 도 넣을 수 잇을 것 같은데...
+}
+
+function WrapWithLabel({ title, children }: PropsWithChildren<labelProps>) {
+  return (
+    <div>
+      <label className="text-xs block mb-2">{title}</label>
+      <div className="flex gap-2 flex-wrap">{children}</div>
+    </div>
+  );
+}
+
+export default WrapWithLabel;
