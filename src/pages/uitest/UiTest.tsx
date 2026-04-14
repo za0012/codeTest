14b51ecf160480eeab472260698e953549d5b95b
za0012 <zinazeek@gmail.com>
2026-04-14 15:30:15 +0900

    ✨ feat: value 삭제 테스트

--- src/pages/uitest/UiTest.tsx
+++ src/pages/uitest/UiTest.tsx
@@ -1,8 +1,38 @@
-import { SelectA, ControlledSelect } from "../../components/custom/SelectC";
+import {
+  SelectA,
+  ControlledSelect,
+  SelectC,
+} from "@/components/custom/SelectC";
+import { useState } from "react";
 
 function UiTest() {
+  const [test, setTest] = useState<string | null>(null);
+  const [selectKey, setSelectKey] = useState(0);
+
+  const handleClear = () => {
+    setTest(null);
+    setSelectKey((prev) => prev + 1); // key 변경 → 리마운트
+  };
+
   return (
     <div className="bg-white h-screen">
+      <div className="w-60 mt-10 ml-10">
+        <SelectC
+          key={selectKey}
+          selectItem={["aaa", "bbb", "ccc", "dddd", "eeee", "ffff", "ggg"]}
+          placeholder="테스트"
+          value={test}
+          onSelect={setTest}
+        />
+        <p className="text-2xl text-blue-500">{test}</p>
+        <button
+          className="bg-violet-400 text-white px-5 py-2 rounded-lg"
+          type="button"
+          onClick={handleClear}
+        >
+          초기화
+        </button>
+      </div>
       <SelectA />
       <ControlledSelect />
     </div>
