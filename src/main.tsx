a10e70b34c030c0d6fb30565b823b4ba9146a22d
za0012 <zinazeek@gmail.com>
2026-04-16 16:29:11 +0900

    feat: tq적용을 위해 StrictMode -> QueryClientProvider으로 교체 및 수정, queryClient 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 // main.tsx
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { routeTree } from "./routeTree.gen"; // 자동 생성 파일
 import "./styles/index.css";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 // 타입 등록
 declare module "@tanstack/react-router" {
@@ -15,7 +17,8 @@ declare module "@tanstack/react-router" {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </StrictMode>,
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
 );
