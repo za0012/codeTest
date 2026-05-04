5054f547372448d57bd110129edea2e20a7a7e14
za0012 <zinazeek@gmail.com>
2026-05-04 13:24:32 +0900

    feat: 프로젝트 시작 시 바로 login 페이지로 나오게끔 설정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
