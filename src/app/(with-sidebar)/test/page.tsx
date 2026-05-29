b729abf6e828280bac28765b11618649572b4325
za0012 <zinazeek@gmail.com>
2026-05-29 17:49:58 +0900

    🚧 chore: 작업중인 파일 동기화를 위해 임시 추가

--- src/app/(with-sidebar)/test/page.tsx
+++ src/app/(with-sidebar)/test/page.tsx
@@ -4,6 +4,10 @@ import { useState } from "react";
 import { AlertCustom } from "@/components/AlertCustom";
 import { useSetAtom } from "jotai";
 import { alertAtom } from "@/lib/store/alertStore";
+import Input from "@/components/ui/Input";
+import SelectDemo from "@/components/SelectCustom";
+import { PLATFORM_TAGS } from "@/constants/problem";
+import { DatePickerDemo } from "@/components/ui/DatePicker";
 // import { useAlert } from "@/util/hook/useAlert";
 
 function page() {
@@ -25,6 +29,45 @@ function page() {
         alert 테스트
       </button>
       {/* <AlertCustom alert={alertData} onClose={closeAlert} /> */}
+      <Input label="Date" size="sm" />
+      {/* 
+      <div className="col-span-1">
+        <select className="w-full bg-[#f9fafb] border-none rounded-xl px-4 py-3 text-[15px] font-medium text-[#4e5968] focus:bg-blue-50/30 focus:ring-2 focus:ring-[#3182f6] outline-none transition-all cursor-pointer">
+          adsa
+        </select>
+        <SelectDemo
+          size={"sm"}
+          variant={"toss"}
+          selectArray={Object.keys(PLATFORM_TAGS)}
+          selectChange={setIsVisible}
+          placeholder="플랫폼"
+        />
+      </div>
+      <DatePickerDemo />
+      <div>
+        <label className="block text-[10px] font-bold text-[#8b95a1] mb-1.5 uppercase">
+          Date
+        </label>
+
+        <input
+          type="date"
+          className="w-full bg-white border border-gray-100 rounded-lg px-2 py-2 text-sm focus:ring-2 focus:ring-[#3182f6] outline-none transition-all"
+        />
+      </div>
+
+      <div className="col-span-1">
+        <label className="flex items-center gap-1.5 text-[11px] font-bold text-[#3182f6] mb-2 uppercase tracking-tight">
+          Platform
+        </label>
+
+        <select className="w-full bg-[#f9fafb] border-none rounded-xl px-4 py-3 text-[15px] font-medium text-[#4e5968] focus:bg-blue-50/30 focus:ring-2 focus:ring-[#3182f6] outline-none transition-all cursor-pointer">
+          <option>백준</option>
+
+          <option>프로그래머스</option>
+
+          <option>LeetCode</option>
+        </select>
+      </div> */}
     </div>
   );
 }
--- src/app/(without-sidebar)/find/component/MakeStudy.tsx
+++ src/app/(without-sidebar)/find/component/MakeStudy.tsx
@@ -1,21 +1,65 @@
-import { ArrowLeft } from "lucide-react";
-import type { phaseType } from "@/lib/types/step";
+// import { ArrowLeft, ArrowRight } from "lucide-react";
+// import type { phaseType } from "@/lib/types/step";
+// import Button from "@/components/ui/Button";
+// import Input from "@/components/ui/Input";
+// import { createStudy } from "@/lib/api/study";
 
-function MakeStudy({ moveStep }: phaseType) {
-  return (
-    <div>
-      <button
-        type="button"
-        onClick={() => moveStep(0)}
-        className="w-full flex items-center gap-2 text-slate-500 hover:text-slate-800 transition-colors text-[16px] font-medium mb-4"
-      >
-        <ArrowLeft size={18} />
+// function MakeStudy({ moveStep }: phaseType) {
+//   const handleCreateStudy = async () => {
+//     try {
+//       await createStudy();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <div>
+//       <button
+//         type="button"
+//         onClick={() => moveStep(0)}
+//         className="w-full flex items-center gap-2 text-slate-500 hover:text-slate-800 transition-colors text-[16px] font-medium mb-2"
+//       >
+//         <ArrowLeft size={18} />
+//         <span>이전으로</span>
+//       </button>
+//       <div className="flex flex-col gap-4 p-6 max-w-md mx-auto">
+//         {/* 1. 메인 텍스트 및 서브 텍스트 영역 */}
+//         <div className="mb-6">
+//           <p className="text-3xl font-extrabold text-slate-900 leading-snug">
+//             초대 코드를
+//             <br /> 입력해주세요
+//           </p>
+//           <p className="text-sm font-medium text-slate-400 mt-2">
+//             스터디장에게 받은 코드를 입력해주세요
+//           </p>
+//         </div>
+//         {/* 2. 초대 코드 입력 영역 */}
+//         <div>
+//           <Input
+//             onChange={(e) => setInviteCode(e.currentTarget.value)}
+//             placeholder="예) ABCD1234"
+//             maxLength={8}
+//             className="text-center text-2xl font-bold text-slate-900 tracking-[0.25em] h-16"
+//           />
+//         </div>
+//         {/* 3. 입장하기 버튼 영역 (사진에 있는 것 추가) */}
+//         <div>
+//           <Button
+//             type="button"
+//             label="스터디 만들기"
+//             onClick={handleCreateStudy}
+//             className="w-full h-15 bg-blue-600 text-white text-[16px] font-bold rounded-2xl flex items-center justify-center gap-2 hover:bg-blue-700 transition-colors"
+//           />
+//         </div>
+//         <div className="bg-blue-200/40 p-4 rounded-2xl mt-4 text-blue-700 font-semibold">
+//           <p className="text-center break-keep">
+//             💡 초대 코드는 스터디장의 마이페이지에서
+//             <br /> 확인할 수 있어요
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-        <span>이전으로</span>
-      </button>
-      MakeStudy
-    </div>
-  );
-}
-
-export default MakeStudy;
+// export default MakeStudy;
