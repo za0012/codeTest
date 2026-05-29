b0dbf4222b2e1241da5f9526e44c466af122febc
za0012 <zinazeek@gmail.com>
2026-05-29 17:43:41 +0900

    💄 feat: 스타일 변경 및 스켈레톤 ui 추가

--- src/app/(with-sidebar)/mypage/page.tsx
+++ src/app/(with-sidebar)/mypage/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import User from "./components/User";
+import { Skeleton } from "@/components/ui/skeleton";
 import { getMyMemberInfo } from "@/lib/api/members";
-import { UserProfile } from "@/lib/types/study";
-import History from "./components/History";
+import type { UserProfile } from "@/lib/types/study";
+import DifficultCount from "./components/BarChart/view";
+import History from "./components/Heatmap/view";
+import MonthlySolve from "./components/LineChart/view";
+import User from "./components/User";
 
 function page() {
   const { data: userInfo } = useQuery<UserProfile>({
@@ -12,14 +15,25 @@ function page() {
     queryFn: getMyMemberInfo,
   });
   return (
-    <div className="flex justify-center">
+    <div className="min-h-screen bg-[#f9fafb]">
       {userInfo ? (
-        <div>
+        <div className="mx-auto flex w-full max-w-245 flex-col gap-6 bg-white py-8 px-8">
           <User userInfo={userInfo} />
           <History id={userInfo.id} />
+          <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+            <MonthlySolve id={userInfo.id} />
+            <DifficultCount id={userInfo.id} />
+          </div>
         </div>
       ) : (
-        <div></div>
+        <div className="mx-auto flex w-full max-w-224.5 flex-col gap-6">
+          <Skeleton className="h-32 w-full max-w-2xl rounded-2xl" />
+          <Skeleton className="h-44 w-full rounded-2xl" />
+          <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+            <Skeleton className="aspect-[1.618] w-full rounded-3xl" />
+            <Skeleton className="aspect-[1.618] w-full rounded-3xl" />
+          </div>
+        </div>
       )}
     </div>
   );
