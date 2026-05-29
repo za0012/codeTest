21b13cb3350292ca06af8906661af325a313eddc
za0012 <zinazeek@gmail.com>
2026-05-29 13:43:55 +0900

    ✨ feat: 데이터 월 형식으로 가공해서 가져오는 것 추가

--- src/lib/api/mypage.ts
+++ src/lib/api/mypage.ts
@@ -1,6 +1,6 @@
 import { supabase } from "../supabase";
 
-export const getHeatmapData = async (memberId: number) => {
+export const getHeatmapDataDaily = async (memberId: number) => {
   const { data, error } = await supabase
     .from("problems")
     .select("date")
@@ -11,6 +11,25 @@ export const getHeatmapData = async (memberId: number) => {
   return data.reduce(
     (acc, { date }) => {
       acc[date] = (acc[date] || 0) + 1;
+      return acc;
+    },
+    {} as Record<string, number>,
+  );
+};
+
+export const getLineGhDataMonth = async (memberId: number) => {
+  const { data, error } = await supabase
+    .from("problems")
+    .select("date")
+    .eq("member_id", memberId);
+  if (error) throw error;
+
+  // { '2026-04-01': 2, '2026-04-02': 1, ... } 형태로 변환
+  return data.reduce(
+    (acc, { date }) => {
+      const cur = new Date(date);
+      const month = cur.toISOString().slice(0, 7);
+      acc[month] = (acc[month] || 0) + 1;
       return acc;
     },
     {} as Record<string, number>,
