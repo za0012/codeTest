4ea0b6aac0c151943a1a5f6f4e1ef5f837e97501
za0012 <zinazeek@gmail.com>
2026-06-02 10:13:06 +0900

    ✨ feat: 아직 사용하진 않으나 혹시 몰라 추후를 위해 배럴파일 생성

---
+++ src/app/(with-sidebar)/settings/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./AccountSetting/view";
+export * from "./AlarmSetting/view";
+export * from "./ProfileSetting/view";
+export * from "./ScreenSetting/view";
+export * from "./StudySetting/view";
+
+//배럴파일을 사용할지 말지 고민
