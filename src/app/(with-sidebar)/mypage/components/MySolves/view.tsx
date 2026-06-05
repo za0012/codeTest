1ecb3083d8c199d6ee563bd31b3b5c8e0cf07afa
za0012 <zinazeek@gmail.com>
2026-06-05 09:10:36 +0900

    ✨ feat: 문제 해결 목록 기능 추가 - 몇 개 풀었는지 확인하는 부분 - openProblem으로 id를 가져온 후 setOpenProblem으로 onClose를 설정하면서 id를 0으로 되돌려 모달을 닫도록 설정

--- src/app/(with-sidebar)/mypage/components/MySolves/view.tsx
+++ src/app/(with-sidebar)/mypage/components/MySolves/view.tsx
@@ -1,12 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import ProblemCard from "@/app/(with-sidebar)/problems/components/ProblemCard";
+import ProblemModal from "@/app/(with-sidebar)/problems/components/ProblemModal";
+import { getMyMemberInfo } from "@/lib/api/members";
 import type { Problem } from "@/lib/types/study";
 import { getMySolves } from "./service";
 
 function MySolves({ id }: { id: number }) {
+  const [openProblem, setOpenProblem] = useState(0);
+
   const { data, isLoading, isError } = useQuery<Problem[]>({
     queryKey: ["mySolves"],
     queryFn: () => getMySolves(id),
+  });
+
+  const { data: myInfo } = useQuery({
+    queryKey: ["myInfo"],
+    queryFn: getMyMemberInfo,
   });
 
   if (isLoading) return <div>Loading...</div>;
@@ -16,21 +26,37 @@ function MySolves({ id }: { id: number }) {
 
   return (
     <div>
-      {data?.map((problem) => (
-        <ProblemCard
-          id={problem.id}
-          key={problem.id}
-          title={problem.title}
-          tags={problem.tags}
-          time_spent={problem.time_spent}
-          difficulty={problem.difficulty}
-          platform={problem.platform}
-          date={problem.date}
-          onClick={(): void => {
-            throw new Error("Function not implemented.");
-          }}
+      <div className="flex justify-between mb-3">
+        <p className="font-bold text-[#191F28] text-base tracking-tight">
+          내가 푼 문제
+        </p>
+        <p className="font-medium text-gray-500 text-sm tracking-tight">
+          {data?.length}개
+        </p>
+      </div>
+      <div className="flex flex-col gap-2 px-4">
+        {data?.map((problem) => (
+          <ProblemCard
+            id={problem.id}
+            key={problem.id}
+            title={problem.title}
+            tags={problem.tags}
+            time_spent={problem.time_spent}
+            difficulty={problem.difficulty}
+            platform={problem.platform}
+            date={problem.date}
+            onClick={() => setOpenProblem(problem.id)}
+            url={problem.url}
+          />
+        ))}
+      </div>
+      {openProblem !== 0 && (
+        <ProblemModal
+          id={openProblem}
+          onClose={setOpenProblem}
+          user_id={myInfo.id}
         />
-      ))}
+      )}
     </div>
   );
 }
