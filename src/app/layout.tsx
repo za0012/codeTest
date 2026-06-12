13ace7be23cba5408f80a6974a3d614e5bb23ea2
za0012 <zinazeek@gmail.com>
2026-06-12 17:22:37 +0900

    ✨ feat: queryClient provider안이 아닌 바깥에서 호출하고 있어서 생기는 오류 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,11 @@ export default function RootLayout({
       // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Providers>{children}</Providers>
-        {alertAtom && <AlertManager />}
-        {modalAtom && <ModalManager />}
+        <Providers>
+          {children}
+          {alertAtom && <AlertManager />}
+          {modalAtom && <ModalManager />}
+        </Providers>
       </body>
     </html>
   );
